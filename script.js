ee8149be20be4f3290b20efda4c26e6e275e3820
Pallavighawat <ghawatpallavi18@gmail.com>
2026-02-23 20:47:36 +0530

    Update script.js
    Update

--- script.js
+++ script.js
@@ -64,3 +64,13 @@ if (form) {
     });
     
 }
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({
+        behavior: 'smooth'
+      });
+  });
+});
